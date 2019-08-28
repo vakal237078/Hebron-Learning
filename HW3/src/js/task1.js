ce72a2078d6d7e7bcb3b43fcd8077a2716817c4a
vakal2395 <vakal237078@gmail.com>
2019-08-28 15:33:27 +0300

    fix bugs

--- HW3/src/js/task1.js
+++ HW3/src/js/task1.js
@@ -1,11 +1,13 @@
-let x = prompt('Введите сумму');
-let y = prompt('Введите процент скидки');
-let seved;
+let sum =+ prompt("Введите сумму");
+let discount =+ prompt("Введите процент скидки");
+let result;
+let saved;
 
-let sum = parseInt(x);
-let discount = parseInt(y);
-
-if (sum !== '' && discount !== ''){
-    seved = (sum/100)*discount;
-    console.log((sum - seved).toFixed(2));
+if (sum !== "" && discount !== ""){
+    result = (sum/100)*discount;
+    saved = sum - result;
+    console.log(`Price without discount: ${sum}\n
+    Discount: ${discount.toString()}%\n
+    Price with discount: ${(saved.toFixed(2)*100)/100}\n
+    Saved: ${(result.toFixed(2)*100)/100}`);
 }
--- HW3/src/js/task2.js
+++ HW3/src/js/task2.js
@@ -1,6 +1,6 @@
-let a = parseFloat(prompt(`Укажите сторону 'a':`));
-let b = parseFloat(prompt(`Укажите сторону 'b':`));
-let alph = parseFloat(prompt(`Укажите угол 'α':`));
+let a =+ prompt("Укажите сторону 'a':");
+let b =+ prompt("Укажите сторону 'b':");
+let alph =+ prompt("Укажите угол 'α':");
 
 function func(a, b, alph) {
     let c = parseFloat(Math.sqrt((Math.pow(a, 2) + Math.pow(b,2)) - (2*(a*b)) * Math.cos(alph * Math.PI / 180)).toFixed(2));
@@ -9,9 +9,9 @@ function func(a, b, alph) {
     let S = Math.sqrt(p*(p-a)*(p-b)*(p-c));
 
 
-    console.log('Сторона с равна: ' + c);
-    console.log('Площадь триугольника: ' + S.toFixed(2));
-    console.log('Периметр триугольника: ' + P.toFixed(2));
+    console.log(`Сторона с равна:  ${c}\n
+    Площадь триугольника: ${Math.round(S)}\n
+   Периметр триугольника: ${P.toFixed(2)}`);
 }
 
 
