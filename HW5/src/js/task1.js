c33252c3c510f8341b5b280361fd5aca4db037c2
vakal2395 <vakal237078@gmail.com>
2019-09-02 00:46:32 +0300

    homework 5 is done

---
+++ HW5/src/js/task1.js
@@ -0,0 +1,30 @@
+let getUserName = prompt("Enter your login");
+let getUserPassword;
+let date = new Date();
+
+if (getUserName === "" || getUserName === null) {
+    alert("Canceled");
+} else if (getUserName.length < 4) {
+    alert("I don't know any users having name length less than 4 symbols");
+} else if (getUserName !== "user") {
+    alert("I don’t know you");
+} else if (getUserName === "user") {
+    getUserPassword = prompt("Enter your password");
+
+    if (getUserPassword === "" || getUserPassword === null) {
+        alert("Canceled");
+    } else if (getUserPassword !== "SuperUser") {
+        alert("Wrong password");
+    } else if (getUserPassword === "SuperUser") {
+        console.log(date.getHours());
+        if (date.getHours() < 20) {
+            alert("Good day!")
+        } else {
+            alert("Good evening!")
+        }
+    }
+}
+
+
+
+
---
+++ HW5/src/js/task2.js
@@ -0,0 +1,79 @@
+let wantUserPlayGame = confirm("Do you want to play a game?");
+let guessedNumber;
+let userResponse;
+let userResponseForNextGame = null;
+let totalPrize = 0;
+let firstNumberForInterval = 5;
+let superPrize = 10;
+let middlePrize = 5;
+let lastPrize = 2;
+
+
+function newGame(levelUp, doublingInterval, upPrize) {
+    let attempts = 3;
+
+    if (levelUp) {
+        firstNumberForInterval = firstNumberForInterval * doublingInterval;
+        superPrize = superPrize + upPrize[0];
+        middlePrize = middlePrize + upPrize[1];
+        lastPrize = lastPrize + upPrize[2];
+    }
+
+    if (wantUserPlayGame) {
+        guessedNumber = Math.floor(Math.random() * firstNumberForInterval) + 1;
+
+        userResponse = +prompt(`
+        Enter a number from 1 to ${firstNumberForInterval}
+        Attempts left: ${attempts}
+        Total prize: ${totalPrize}$
+        Possible prize on current attempts: ${superPrize}$`);
+
+        if (userResponse === guessedNumber) {
+            totalPrize += superPrize;
+            userResponseForNextGame = confirm(`Congratulation! Your prize is: ${superPrize}$ Do you want to continue?`);
+            nextGame();
+        } else {
+            userResponse = +prompt(`
+             Enter a number from 1 to ${firstNumberForInterval}
+             Attempts left: ${attempts -= 1}
+             Total prize: ${totalPrize}$
+             Possible prize on current attempts: ${middlePrize}$`);
+
+            if (userResponse === guessedNumber) {
+                totalPrize += middlePrize;
+                userResponseForNextGame = confirm(`Congratulation! Your prize is: ${middlePrize}$ Do you want to continue?`);
+                nextGame();
+            } else {
+                userResponse = +prompt(`
+                 Enter a number from 1 to ${firstNumberForInterval}
+                 Attempts left: ${attempts -= 1}
+                 Total prize: ${totalPrize}$
+                 Possible prize on current attempts: ${lastPrize}$`);
+
+                if (userResponse === guessedNumber) {
+                    totalPrize += lastPrize;
+                    userResponseForNextGame = confirm(`Congratulation! Your prize is: ${lastPrize}$ Do you want to continue?`);
+                    nextGame();
+                } else {
+                    alert(`Thank you for a game. Your prize is: ${totalPrize}$`)
+                }
+            }
+        }
+    } else {
+        alert("You did not become a millionaire, but can.")
+    }
+}
+
+function nextGame() {
+    if (userResponseForNextGame) {
+        newGame(true, 2, [20, 10, 5]);
+
+    } else if (userResponseForNextGame === null) {
+        newGame();
+
+    } else {
+        alert(`Thank you for a game. Your prize is: ${totalPrize}$`)
+    }
+}
+
+nextGame();
