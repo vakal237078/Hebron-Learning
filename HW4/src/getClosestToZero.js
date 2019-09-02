7dcfb72c62a08d390b66303d52b472e2401fa74f
vakal2395 <vakal237078@gmail.com>
2019-09-02 10:48:16 +0300

    fix bugs

--- HW4/src/getClosestToZero.js
+++ HW4/src/getClosestToZero.js
@@ -1,3 +1,3 @@
-const closest = (array) => array.sort( (a, b) => Math.abs(0 - a) - Math.abs(0 - b))[0];
+const closest = (...arguments) => arguments.sort((a, b) => Math.abs(0 - a) - Math.abs(0 - b))[0];
 
-console.log(closest([-3, -2, 6, 8, -1, 1, 12]));
+console.log(closest(-3, -2, 6, 8, -1, 1, 12));
